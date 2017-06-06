5c4c4bf353d56bb907b169150cfff454ce03cc01
Eric Kinoshita <eric.void@gmail.com>
2017-06-06 01:03:29 -0300

    git initial commit, added main search view and list view

---
+++ app/src/main/java/com/ericvoid/githubprofileviewer/MainActivity.java
@@ -0,0 +1,26 @@
+package com.ericvoid.githubprofileviewer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button searchButton = (Button)findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UserProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
